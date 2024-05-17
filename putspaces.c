d74617054fc203d6afef01c6148ba715ce0b1c26
Zainab Ait Belaid <zait-bel@e2r10p5.1337.ma>
2024-05-17 15:03:02 +0100

    last modifications

--- putspaces.c
+++ putspaces.c
@@ -16,11 +16,13 @@ char	*ft_puspaces(char *av)
 {
 	int i;
 	int j;
+	int	c;
 	int len;
 	char *new_str;
 
 	i = 0;
 	j = 0;
+	c = 0;
 	len = ft_strlen(av);
 	new_str = malloc(len + 1);
 	if (!new_str)
@@ -34,23 +36,30 @@ char	*ft_puspaces(char *av)
 	{
 		if(av[i] == 39 || av[i] == 34)
 		{
-			i++;
-			while(av[i] != 39 && av[i] != 34)
+			new_str[j++] = av[i++];
+			while(av[i] != 39 && av[i] != 34 && av[i])
+				new_str[j++] = av[i++];
+			if(!av[i])
+				(write(2, "syntax error!\n", 15), exit(1));
+		}
+		while(av[i] != ' ' && !(av[i] >= 9 && av[i] <= 13) && av[i])
+			{
+				new_str[j++] = av[i];
 				i++;
-		}
-		if((av[i] != 32 && !(av[i] >= 9 && av[i] <= 13) && av[i] ))
-		{
-			if (av[i - 1] == 32 || (av[i - 1] >= 9 && av[i - 1] <= 13))
-				new_str[j++] = 32;
-			new_str[j++] = av[i];
-		}
-		i++;
+			}
+			while(av[i] == ' ' || av[i] >= 9 && av[i] <= 13)
+			{
+				c = 1;
+				i++;
+			}
+			if(c == 1 && av[i])
+				(new_str[j++] = 32, c = 0);
 	}
-	return(new_str + 1);
+	return(new_str);
 }
 
 int main()
-{
-	char *str = "heloo           '     kak'     ls cat      gg     k  ";
-	printf("%s\n", ft_puspaces(str));
+{ 
+	char *str = "                   heloo               kak     ls cat      gg     k        laak    llll  llll   ";
+	printf("===%s\n", ft_puspaces(str));
 }
